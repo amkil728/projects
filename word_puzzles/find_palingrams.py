86dd445392db8a66bc87b8eaa77bb42e9f387077
amkil728 <84015075+amkil728@users.noreply.github.com>
2021-05-11 16:13:15 +0530

    Update find_palingrams.py

--- word_puzzles/find_palingrams.py
+++ word_puzzles/find_palingrams.py
@@ -1,6 +1,33 @@
-# find_palingrams.py
+'''
+find_palingrams.py
+
+Find all two word palingrams that can be formed from a list of words. A palingram
+is a palindromic phrase, i.e., a phrase that reads the same when read backwards as forwards.
+E.g., pull up, stir grits, etc.
+Notice that each such phrase has a core word which consists of two parts, a palindromic
+part, and the other part, which, when reversed forms a word.
+E.g., in 'pull up', the core word is 'pull', which can be divided into 'll', which is
+palindromic, and 'pu', which when reversed gives 'up', which is a word. And in 'stir grits',
+the core word is 'grits', which can be divided into 'g', which is a single letter, and hence
+palindromic, and 'rits', when when reversed gives 'stir'.
+
+@author amkil728
+@version 10/04/21
+'''
 
 def find_palingram(wd, word_list):
+	'''Finds all palingrams that can be formed from the given list of words, using the
+	given word as the core word.
+	
+	Arguments:
+	  - wd: word to use as core word
+	  - word_list: list of words
+	
+	Returns:
+		A list of pairs which contain the given word, and other word forming a
+		palingram, if any can be formed;
+		False, otherwise
+	'''
 	wd_len = len(wd)
 	rev_wd = wd[::-1]
 	for i in range(1, wd_len+1):
@@ -20,8 +47,12 @@ def find_palingram(wd, word_list):
 
 
 def find_all_palingrams(word_list):
+	'''Finds all palingrams that can be formed from the list of words.
+	
+	Returns: list of pairs representing palingrams'''
         palingrams = list()
         for word in word_list:
                 phrase = find_palingram(word, word_list)
                 if phrase:
                         palingrams.append(phrase)
+	return palingrams
